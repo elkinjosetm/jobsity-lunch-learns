f9d6eb27a6f04764ad8835d79bf7933e307cb859
Elkin Torres <elkinjosetm@gmail.com>
2019-03-26 12:45:36 -0500

    Update home page

--- react-performance/src/js/pages/home/index.js
+++ react-performance/src/js/pages/home/index.js
@@ -4,8 +4,8 @@ class Home extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<h2>Home</h2>
-				<p>Ad duis esse occaecat commodo nostrud deserunt. Adipisicing eiusmod deserunt ea fugiat consectetur dolor minim sint ut anim do eiusmod et nostrud. In ad exercitation eiusmod amet qui exercitation esse ipsum elit elit non occaecat tempor incididunt.</p>
+				<h2>Welcome to React + Performance playground</h2>
+				<p>A couple of example used to showcase some of the most common performance issues in an applications</p>
 			</React.Fragment>
 		);
 	}
